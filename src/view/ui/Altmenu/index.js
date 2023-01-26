a1ac9ad6b3260474456ba6af44b7797f9b80cf12
John Hudson <john.hudson@seequestor.com>
2023-01-26 20:35:40 +0000

    Added NiceSelect functionality, still no filter though

--- src/view/ui/Altmenu/index.js
+++ src/view/ui/Altmenu/index.js
@@ -13,7 +13,12 @@ export default class Altmenu extends React.Component {
 	render(){
 		return (
 			<div className="altmenu">
+				<span className="logo">
+					<span className="color-alt">&lt;</span>&nbsp;johnhudson&nbsp;<span className="color-alt">/&gt;</span>
+				</span>
+				<button className="git" onClick={ (ev) => { window.open('https://github.com/hudson1998x/2D-To-3D-Pose-Estimation' , 'about:blank;'); }}><i className="fab fa-github"></i></button>
 
+				<span className="right-message">Version 1.1, Beta (Some functionality may not work)</span>
 			</div>
 		)
 	}
